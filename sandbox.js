0a6586d431f1f735bf22f2a1e3b98068acdb53af
wraithM17 <37100541+wraithM17@users.noreply.github.com>
2020-11-21 14:00:47 +0530

    added mobile design

--- sandbox.js
+++ sandbox.js
@@ -1,20 +1,49 @@
-
+var htmlInput = document.querySelector('.htmlInput');
+var cssInput = document.querySelector('.CSSInput');
 
 function runCode(){
-	var textareaObj = document.getElementById('codeInput');
+	if (isMobile){
+		console.log("MOBILE DEVICE");
+		$("#code").hide();
+		$("#sandbox").show();
+	}
+	var textareaObj = document.querySelector('.htmlInput');
+	
 	var code = textareaObj.value.replace(/(\n)/gm," ");
+	console.log(code)
 	var outputObj = document.getElementById('output');
 	output.innerHTML = code;
-
+	var styleObj = document.querySelector('.cssInput');
+	let styles = styleObj.value;
+	output.innerHTML += `<style>${styles}</style>`;
 
 
 }
 
-/*
-textareaObj.addEventListener('keydown',function(r){
 
-	if (e.ctrlKey){
-		runCode();
+function codeHTML() {
+	let update = document.getElementById('language');
+	update.innerHTML = "HTML";
+	$(".cssInput").hide();
+	$(".htmlInput").show();
+}
+
+
+
+function codeCSS(){
+	let update = document.getElementById('language');
+	update.innerHTML = "CSS";
+	$(".cssInput").show();
+	$(".htmlInput").hide();
+}
+
+
+function isMobile(){
+	
+	if (window.innerWidth < 748){
+		
+		return true;
+
 	}
-})
-*/
+}
+
