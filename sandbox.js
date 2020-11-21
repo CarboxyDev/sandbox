d00f0d2309b3ad78af262874307ba67d6d5ea10d
wraithM17 <37100541+wraithM17@users.noreply.github.com>
2020-11-21 15:40:58 +0530

    Added full functionality for mobile devices

--- sandbox.js
+++ sandbox.js
@@ -2,15 +2,16 @@ var htmlInput = document.querySelector('.htmlInput');
 var cssInput = document.querySelector('.CSSInput');
 
 function runCode(){
-	if (isMobile){
+	if (isMobile()){
 		console.log("MOBILE DEVICE");
 		$("#code").hide();
 		$("#sandbox").show();
+		$("#revert").show();
 	}
 	var textareaObj = document.querySelector('.htmlInput');
 	
 	var code = textareaObj.value.replace(/(\n)/gm," ");
-	console.log(code)
+	
 	var outputObj = document.getElementById('output');
 	output.innerHTML = code;
 	var styleObj = document.querySelector('.cssInput');
@@ -39,11 +40,23 @@ function codeCSS(){
 
 
 function isMobile(){
-	
-	if (window.innerWidth < 748){
-		
+	var clientWidth = $(window).width();
+
+	if (clientWidth < 748){
 		return true;
 
 	}
+	else if (clientWidth >= 748){
+
+		return false;
+	}
 }
 
+
+
+function revertMobile(){
+	$("#revert").hide();
+	$("#sandbox").hide();
+	$("#code").show();
+
+}
